1ad56c642575a8ac1d628ab472d190f874299a1e
Vasmaster <vasil.grancharov1996@gmail.com>
2025-01-28 22:47:28 +0200

    V1.0

--- functions/index.js
+++ functions/index.js
@@ -6,44 +6,9 @@
  *
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
-import { firestore } from 'firebase-functions';
-import { initializeApp, firestore as _firestore } from 'firebase-admin';
-import { onRequest } from "firebase-functions/v2/https";
-import logger from "firebase-functions/logger";
-initializeApp();
 
-export const aggregateData = firestore
-  .document('participants/{participantId}')
-  .onCreate(async (snapshot, context) => {
-    const data = snapshot.data();
-
-    // Calculate averages (example)
-    const participants = await _firestore().collection('participants').get();
-    let totalHappiness = 0;
-    let totalSadness = 0;
-    let totalCalmness = 0;
-    let totalFrustration = 0;
-    participants.forEach(doc => {
-      totalHappiness += doc.data().happiness;
-      totalSadness += doc.data().sadness;
-      totalCalmness += doc.data().calmness;
-      totalFrustration += doc.data().frustration;
-    });
-
-    const avgHappiness = totalHappiness / participants.size;
-    const avgSadness = totalSadness / participants.size;
-    const avgCalmness = totalCalmness / participants.size;
-    const avgFrustration = totalFrustration / participants.size;
-
-    // Save averages to Firestore
-    await _firestore().collection('averages').doc('current').set({
-      avgHappiness,
-      avgSadness,
-      avgCalmness,
-      avgFrustration,
-      timestamp: _firestore.FieldValue.serverTimestamp()
-    });
-  });
+const {onRequest} = require("firebase-functions/v2/https");
+const logger = require("firebase-functions/logger");
 
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
